72c3914a2f41b38b71f6ad744a7be975c01af2b3
Andej Nesterenko <mentatus.n@gmail.com>
2021-12-17 21:19:31 +0300

    Урок 1 начали суть истории возникновения котлина разобрали классы( констуркторы primary и перегреженные + init{}) и функции(как внутренние, так и высшего уровня) var val модификаторы (изменяемое или не изменяемое) разобрали ньюансы наследования в котлин ( нужен явный open) именованные параметры

--- app/src/main/java/com/gb/kotlin_1728_2_1/MainActivity.kt
+++ app/src/main/java/com/gb/kotlin_1728_2_1/MainActivity.kt
@@ -1,14 +1,18 @@
-package com.gb.kotlin_1728_2_1;
+package com.gb.kotlin_1728_2_1
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val person = Person(age=20,name="Pavel",bio="bidsewgerswg", bio2 ="sdgsgr")
+        val person2 = Person("Pavel",12,"bio","bio2")
+        Log.d("mylogs","${person.name}")
+        Log.d("mylogs","$test")
+        test = "NewTest"
+        Log.d("mylogs","$test")
     }
 }
