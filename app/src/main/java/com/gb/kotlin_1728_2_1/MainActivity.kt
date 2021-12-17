072f8d4d77ce39e018f4cbc581adadf5fb94997e
Andej Nesterenko <mentatus.n@gmail.com>
2021-12-17 21:36:42 +0300

    Урок 1 разобрались с lateinit

--- app/src/main/java/com/gb/kotlin_1728_2_1/MainActivity.kt
+++ app/src/main/java/com/gb/kotlin_1728_2_1/MainActivity.kt
@@ -7,12 +7,14 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // TODO создать проект в гит
         setContentView(R.layout.activity_main)
         val person = Person(age=20,name="Pavel",bio="bidsewgerswg", bio2 ="sdgsgr")
         val person2 = Person("Pavel",12,"bio","bio2")
-        Log.d("mylogs","${person.name}")
-        Log.d("mylogs","$test")
+        Log.d("mylogs", person.name)
+        Log.d("mylogs", person.newProperty)
+        Log.d("mylogs", test)
         test = "NewTest"
-        Log.d("mylogs","$test")
+        Log.d("mylogs", test)
     }
 }
--- app/src/main/java/com/gb/kotlin_1728_2_1/Person.kt
+++ app/src/main/java/com/gb/kotlin_1728_2_1/Person.kt
@@ -1,16 +1,16 @@
 package com.gb.kotlin_1728_2_1
 
 open class Person constructor(val name:String,var age:Int=25,var bio:String="",var bio2:String="") {
-    init {
-       // name = "initName"
-        age = 30
-    }
+
+    lateinit var newProperty:String
+
+
     fun foo(name: String):Int {
         return age
     }
 }
 
-class Student(var group:Int,name:String,age:Int):Person(name,age){
+class Student(var group:Int,name:String,age:Int): Person(name,age){
 
 }
 
