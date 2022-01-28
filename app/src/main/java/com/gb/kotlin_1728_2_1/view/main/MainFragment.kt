4ed19888ed0f69d1e7ba8aa54b50630c78f36ce8
Andej Nesterenko <mentatus.n@gmail.com>
2022-01-28 21:03:05 +0300

    урок 10 начали, добавили запрос разрешений. Добавили получение координат

--- app/src/main/java/com/gb/kotlin_1728_2_1/view/main/MainFragment.kt
+++ app/src/main/java/com/gb/kotlin_1728_2_1/view/main/MainFragment.kt
@@ -1,9 +1,19 @@
 package com.gb.kotlin_1728_2_1.view.main
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -48,7 +58,125 @@ class MainFragment : Fragment(), OnMyItemClickListener {
             mainFragmentFAB.setOnClickListener {
                 sentRequest()
             }
+            mainFragmentFABLocation.setOnClickListener {
+                checkPermission()
+            }
         }
+    }
+
+    private fun checkPermission() {
+        context?.let {
+            when {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    getLocation()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                    showDialogRatio()
+                }
+                else -> {
+                    myRequestPermission()
+                }
+            }
+        }
+    }
+
+
+    private val MIN_DISTANCE = 100f
+    private val REFRESH_PERIOD = 60000L
+
+    private fun getAddress(location: Location){
+        Log.d(""," $location")
+        /*Thread{
+            val geocoder = Geocoder(requireContext())
+            val listAddress=geocoder.getFromLocation(location.latitude,location.longitude,1)
+        }.start()*/
+    }
+    private val locationListener = object : LocationListener{
+        override fun onLocationChanged(location: Location) {
+            getAddress(location)
+        }
+        override fun onProviderDisabled(provider: String) {
+            super.onProviderDisabled(provider)
+        }
+        override fun onProviderEnabled(provider: String) {
+            super.onProviderEnabled(provider)
+        }
+    }
+
+
+    private fun getLocation(){
+        activity?.let {
+            if(ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )==PackageManager.PERMISSION_GRANTED){
+                val locationManager = it.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                    val providerGPS = locationManager.getProvider(LocationManager.GPS_PROVIDER)
+                    providerGPS?.let {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            REFRESH_PERIOD,
+                            MIN_DISTANCE,
+                            locationListener
+                        )
+                    }
+                }else{
+                    val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    lastLocation?.let{
+                        getAddress(it)
+                    }
+                }
+            }else{
+
+            }
+        }
+    }
+
+    private fun showDialog(){
+
+    }
+
+    val REQUEST_CODE = 999
+    private fun myRequestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE) {
+
+            when {
+                (grantResults[0] == PackageManager.PERMISSION_GRANTED) -> {
+                    getLocation()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                    showDialogRatio()
+                }
+                else -> {
+                    Log.d("", "КОНЕЦ")
+                }
+            }
+        }
+    }
+
+    private fun showDialogRatio() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Доступ к геолокации") // TODO HW
+            .setMessage(getString(R.string.dialog_message_no_gps))
+            .setPositiveButton("Предоставить доступ") { _, _ ->
+                myRequestPermission()
+            }
+            .setNegativeButton("Не надо") { dialog, _ -> dialog.dismiss() }
+            .create()
+            .show()
+
     }
 
     private fun sentRequest() {
